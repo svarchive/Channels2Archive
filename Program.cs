b80f6ea8a14a656139631b8fc68b2246cbe24b81
Coca162 <coca16622@gmail.com>
2021-01-18 13:50:00 +0000

    Generally better (idk i haven't updated in a while)

--- Program.cs
+++ Program.cs
@@ -13,48 +13,40 @@ namespace Channels2Archive
             while (true)
             {
                 Console.WriteLine("Input target directory: ");
-                string target = Console.ReadLine();
-                Dictionary<int, Tuple<string, string>> channels = new Dictionary<int, Tuple<string, string>>();
-                int num = 0;
-                List<string> catogories = new List<string>();
-                string[] html = Directory.GetFiles(target, "*.html");
+                string target = Console.ReadLine() + @"\";
+                Console.WriteLine("Input github static page directory: ");
+                string gitdir = Console.ReadLine();
+                if (gitdir.Length != 0) gitdir += "/";
+                Dictionary<string, string> channels = new();
+                List<string> categorizes = new();
+                string[] html = Directory.GetFiles(target ?? throw new ArgumentException("target is not valid!"), "*.html");
                 foreach (string pathName in html)
                 {
                     string full = pathName.Split('/').Last();
                     string[] split = full.Split('[')[0].Split('-');
-                    var catogory = split[1].Trim();
-                    var nameList = split.Skip(2);
+                    string category = split[1].Trim();
+                    IEnumerable<string> nameList = split.Skip(2);
                     string name = string.Join('-', nameList).Replace(" ", string.Empty);
-                    bool isname = false;
-                    foreach (var channel in channels) if (channel.Value.Item1 == name) isname = true;
-                    if (isname != true) File.Move(pathName, $"{target}{name}.html");
-                    else
-                    {
-                        name = $"{catogory} {name}";
-                        File.Move(pathName, $"{target}{name}.html");
-                    }
-                    channels.Add(num, Tuple.Create(name, catogory));
-                    num += 1;
-                    if (!catogories.Contains(catogory)) catogories.Add(catogory);
+                    if (channels.ContainsKey(name)) name = $"{category} {name}";
+                    File.Move(pathName, $"{target}{name}.html");
+                    channels.Add(name, category);
+                    if (!categorizes.Contains(category)) categorizes.Add(category);
                 }
 
                 string final = "";
-                foreach (string catogory in catogories)
+                foreach (string category in categorizes)
                 {
-                    final += $"\n## {catogory}";
-                    foreach (var channel in channels)
+                    final += $"\n## {category}";
+                    foreach ((string key, string _) in channels.Where(channel => channel.Value == category))
                     {
-                        if (channel.Value.Item2 == catogory)
-                        {
-                            final += $"\n\n### [{channel.Value.Item1}](https://svarchive.github.io{target.Split("svarchive.github.io")[1].Replace(@"\", "/")}{channel.Value.Item1})";
-                            channels.Remove(channel.Key);
-                        }
+                        final += $"\n\n### [{key}](https://svarchive.github.io/{gitdir}{key})";
+                        channels.Remove(key);
                     }
                 }
 
                 File.WriteAllText(target + "server.md", final);
                 channels.Clear();
-                catogories.Clear();
+                categorizes.Clear();
             }
         }
     }
